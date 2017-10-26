510dec9fef4b671424525c4c3b7bc028c14ea1c6
Katie Schermerhorn <katieschermerhorn@katies-air.dhcp.nd.edu>
2017-10-26 15:14:01 -0400

    m

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -4,15 +4,15 @@
 from flask import Flask, render_template, request
 
 import sys
-import MySQLdb
+# import MySQLdb
 
-# Global Variables
+# # Global Variables
 
-# Database info
-SQL_ADDR= 'localhost'
-SQL_USER= 'kscherme'
-SQL_PASSWD= 'crossroads111'
-SQL_DB= 'crossroads'
+# # Database info
+# SQL_ADDR= 'localhost'
+# SQL_USER= 'kscherme'
+# SQL_PASSWD= 'crossroads111'
+# SQL_DB= 'crossroads'
 
 # Flask info
 app = Flask(__name__)
@@ -21,9 +21,9 @@ if __name__ == "__main__":
 	app.run(host='dsg1.crc.nd.edu',port=5200)
 
 # Setup Database
-db = MySQLdb.connect(host=SQL_ADDR, user=SQL_USER, passwd=SQL_PASSWD, db=SQL_DB)
+# db = MySQLdb.connect(host=SQL_ADDR, user=SQL_USER, passwd=SQL_PASSWD, db=SQL_DB)
 
-cursor = db.cursor;
+# cursor = db.cursor;
 
 # Functions
 
