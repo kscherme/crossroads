3c34855548310759f68110276fffc4a2218d3ee6
Katie Schermerhorn <katieschermerhorn@katies-air.dhcp.nd.edu>
2017-10-26 15:07:59 -0400

    inserting

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,13 +1,53 @@
 #!/usr/bin python
+
+# Libraries
 from flask import Flask, render_template
 
+import sys
+import MySQLdb
+
+# Global Variables
+
+# Database info
+SQL_ADDR= 'localhost'
+SQL_USER= 'kscherme'
+SQL_PASSWD= 'crossroads111'
+SQL_DB= 'crossroads'
+
+# Flask info
 app = Flask(__name__)
-@app.route("/")
-def main():
-	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(host='dsg1.crc.nd.edu',port=5200)
 
-#help
+# Setup Database
+db = MySQLdb.connect(host=SQL_ADDR, user=SQL_USER, passwd=SQL_PASSWD, db=SQL_DB)
+
+cursor = db.cursor;
+
+# Functions
+
+# Inserts a new movie into the database
+def insertMovieDB(year, title, mid):
+	# Format SQL
+	sql = 'INSERT INTO movies (year, title, movieid) VALUES ("{}", "{}", "{}")'.format(year, title, mid)
+	# Execute SQL
+	cursor.execute(sql)
+	db.commit()
+	return True
+
+# Flask templates
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+@app.route("/insertMovie", methods=['POST'])
+def insertMovie():
+	if request.form['submit'] == 'POST':
+		movieName = request.form['movieTitle']
+		movieYear = request.form['movieYear']
+		movieID = request.form['mid']
+		insertMovieDB(movieYear, movieName, movieID)
+
+
 
