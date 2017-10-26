fd26eac246d818d3e0a1b404785c82f8adcee44b
Katie Schermerhorn <katieschermerhorn@katies-air.dhcp.nd.edu>
2017-10-26 16:21:41 -0400

    m

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -17,22 +17,6 @@ SQL_DB= 'crossroads'
 # Flask info
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return render_template('index.html')
-
-@app.route("/insert", methods=['POST','GET'])
-def insert():
-	if request.form['submit'] == 'POST':
-		movieName = request.form['movieTitle']
-		movieYear = request.form['movieYear']
-		movieID = request.form['mid']
-		insertMovieDB(movieYear, movieName, movieID)
-		return render_template("insert.html", name=movieName, year=movieYear, mid=movieID)
-
-if __name__ == "__main__":
-	app.run(host='dsg1.crc.nd.edu',port=5201,debug=True)
-
 # Setup Database
 db = MySQLdb.connect(host=SQL_ADDR, user=SQL_USER, passwd=SQL_PASSWD, db=SQL_DB)
 
@@ -50,6 +34,22 @@ def insertMovieDB(year, title, mid):
 	return True
 
 # Flask templates
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+@app.route("/insert", methods=['POST','GET'])
+def insert():
+	if request.form['submit'] == 'POST':
+		movieName = request.form['movieTitle']
+		movieYear = request.form['movieYear']
+		movieID = request.form['mid']
+		insertMovieDB(movieYear, movieName, movieID)
+		return render_template("insert.html", name=movieName, year=movieYear, mid=movieID)
+
+if __name__ == "__main__":
+	app.run(host='dsg1.crc.nd.edu',port=5201,debug=True)
 
 # @app.route("/insert", methods=['POST','GET'])
 # def insert():
