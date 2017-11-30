76a368e8ba6fc0f6ea23822dcd4860b2aa52c1b5
Katie Schermerhorn <katieschermerhorn@katies-air.dhcp.nd.edu>
2017-11-30 16:28:37 -0500

    add like button to search

--- temp/app.py
+++ temp/app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 # Setup Database
 db = MySQLdb.connect(host=SQL_ADDR, user=SQL_USER,
-                     passwd=SQL_PASSWD, db=SQL_DB)
+	passwd=SQL_PASSWD, db=SQL_DB)
 
 cursor = db.cursor()
 
@@ -40,7 +40,7 @@ def insertMovieDB(year, title, is_tv):
 
 # Searches for movie by title
 def searchMovieDB(title):
-    	title = "%" + title + "%"
+	title = "%" + title + "%"
 	# Format SQL
 	sql = 'SELECT movieID, title, year FROM Movies WHERE Title LIKE "{}" and is_tv = 1'.format(
 		title)
@@ -51,13 +51,13 @@ def searchMovieDB(title):
 	# Return results
 	return tuples
 
-def searchUserDB(user):
-	user = "%" + user + "%"
+	def searchUserDB(user):
+		user = "%" + user + "%"
         # Format SQL
-	if user is not "%%":
+        if user is not "%%":
         	sql = 'SELECT * FROM Users WHERE username LIKE "{}"'.format(user)
-	else:
-		sql = 'SELECT * FROM Users'
+        else:
+        	sql = 'SELECT * FROM Users'
         # Execute SQL
         cursor.execute(sql)
         # Collect Results
@@ -101,7 +101,7 @@ def updateMovieRating(movieID, userRating):
 	else:
 		return "No MovieID"
 
-def authenticate(input_username, input_password):
+		def authenticate(input_username, input_password):
 	# Format SQL
 	sql = 'SELECT id, username, password FROM Users WHERE username = "{}" and password = "{}"'.format(
 		input_username, input_password)
@@ -112,7 +112,7 @@ def authenticate(input_username, input_password):
 	if tuple:
 		result = tuple[0][0]
 	else:
-    		result = False
+		result = False
 	# Return results
 	if result:
 		user.username = input_username
@@ -122,7 +122,7 @@ def authenticate(input_username, input_password):
 	else:
 		return False
 
-def createUser(input_username, input_password):
+		def createUser(input_username, input_password):
 	# Format SQL to Check for Other Users
 	sql = 'SELECT * FROM Users WHERE username = "{}"'.format(input_username)
 	#Execute SQL
@@ -143,118 +143,118 @@ def createUser(input_username, input_password):
 	#Collect Results
 	tuple = cursor.fetchall()
 	id = 0
-    if tuple:
-    	id = tuple[0][0]
-	
+	if tuple:
+		id = tuple[0][0]
+		
 	# Set User
 	user.username = input_username
 	user.password = input_password
 	user.id = id
-    return True    	
+	return True    	
 
-def setFollowingUser(userToFollow):
+	def setFollowingUser(userToFollow):
 	# Format SQL to Check if Already Following
 	sql = 'SELECT * FROM Following WHERE Follower = "{}" and Following = "{}"'.format(user.username, userToFollow)
 	#Execute SQL
-    cursor.execute(sql)
+	cursor.execute(sql)
     # Collect Results
     tuple = cursor.fetchall()
     if tuple:
     	return False
 
-	if user.username == userToFollow:
-		return False
- 
+    	if user.username == userToFollow:
+    		return False
+    		
 	# Format SQL to Set Following
 	sql = 'INSERT INTO Following (Follower, Following) VALUES ("{}", "{}")'.format(user.username, userToFollow)
 	# Execute SQL
-    cursor.execute(sql)
-    db.commit()
-    return True
+	cursor.execute(sql)
+	db.commit()
+	return True
 
 # User Class
 class User(object):
 	def __init__(self, username, password):
-			self.username = username
-			self.password = password
+		self.username = username
+		self.password = password
 # Flask templates
 user = User("","")
 
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	else:
 		print user.id
 		return redirect(url_for('homepage'))
 
-@app.route('/adv_search')
-def adv_search():
-	return render_template('adv_search.html')
+		@app.route('/adv_search')
+		def adv_search():
+			return render_template('adv_search.html')
 
-@app.route('/login', methods=['POST'])
-def do_login():
-    if request.method == 'POST':
+			@app.route('/login', methods=['POST'])
+			def do_login():
+				if request.method == 'POST':
     #if request.form['password'] == 'password' and request.form['username'] == 'admin':
-		username = request.form['username']
-		password = request.form['password']
-		if (authenticate(username, password)):
-			session['logged_in'] = True
-		else:
-			flash('wrong password!')
-    return home()
+    username = request.form['username']
+    password = request.form['password']
+    if (authenticate(username, password)):
+    	session['logged_in'] = True
+    else:
+    	flash('wrong password!')
+    	return home()
 
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return home()
+    	@app.route("/logout")
+    	def logout():
+    		session['logged_in'] = False
+    		return home()
 
-@app.route("/homepage")
-def homepage():
-	return render_template('homepage.html')
+    		@app.route("/homepage")
+    		def homepage():
+    			return render_template('homepage.html')
 
-@app.route("/create_user", methods=['POST','GET'])
-def create_user():
-    if request.method == 'POST':
-		username = request.form['username']
-        password = request.form['password']
-        if (createUser(username, password)):
-                return do_login()
-    
-    return render_template('create_user.html')
+    			@app.route("/create_user", methods=['POST','GET'])
+    			def create_user():
+    				if request.method == 'POST':
+    					username = request.form['username']
+    					password = request.form['password']
+    					if (createUser(username, password)):
+    						return do_login()
+    						
+    						return render_template('create_user.html')
 
-@app.route("/insert", methods=['POST', 'GET'])
-def insert():
-	if request.method == 'POST':
-		movieName = request.form['movieTitle']
-		movieYear = request.form['movieYear']
-		is_tv = request.form['is_tv']
-		insertMovieDB(movieYear, movieName, is_tv)
-		return render_template("insert.html", name=movieName, year=movieYear)
-	else:
-		return render_template("insert.html", name="", year="")
+    						@app.route("/insert", methods=['POST', 'GET'])
+    						def insert():
+    							if request.method == 'POST':
+    								movieName = request.form['movieTitle']
+    								movieYear = request.form['movieYear']
+    								is_tv = request.form['is_tv']
+    								insertMovieDB(movieYear, movieName, is_tv)
+    								return render_template("insert.html", name=movieName, year=movieYear)
+    							else:
+    								return render_template("insert.html", name="", year="")
 
 
-@app.route("/search", methods=['POST', 'GET'])
-def search():
-    tuples = []
-	if request.method == 'POST':
-		searchMovie = request.form['movieSearch']
-		tuples = searchMovieDB(searchMovie)
-		if tuples:
-			return render_template("search.html", tuples=tuples)
-		else:
-    			return render_template("search.html", tuples=None)
-	else:
-    		return render_template("search.html", tuples=tuples)
+    								@app.route("/search", methods=['POST', 'GET'])
+    								def search():
+    									tuples = []
+    									if request.method == 'POST':
+    										searchMovie = request.form['movieSearch']
+    										tuples = searchMovieDB(searchMovie)
+    										if tuples:
+    											return render_template("search.html", tuples=tuples)
+    										else:
+    											return render_template("search.html", tuples=None)
+    										else:
+    											return render_template("search.html", tuples=tuples)
 
-@app.route("/follow", methods=['POST', 'GET'])
-def follow():
-	tuples = []
-	if request.method == 'POST':
-		if request.form['submit'] == 'Follow':
-                        username =  request.form['username']
-			setFollowingUser(username)
+    											@app.route("/follow", methods=['POST', 'GET'])
+    											def follow():
+    												tuples = []
+    												if request.method == 'POST':
+    													if request.form['submit'] == 'Follow':
+    														username =  request.form['username']
+    														setFollowingUser(username)
 			#return render_template("follow.html", tuples=tuples)
 		#else:
 		if request.form['submit'] == 'SEARCH ALL USERS':
@@ -264,13 +264,13 @@ def follow():
 			searchUser = request.form['userSearch']
 			tuples = searchUserDB(searchUser)
 
-               	if tuples:
-                      	return render_template("follow.html", tuples=tuples)
-                else:
-                        return render_template("follow.html", tuples=None)
+			if tuples:
+				return render_template("follow.html", tuples=tuples)
+			else:
+				return render_template("follow.html", tuples=None)
 
-	else:
-                return render_template("follow.html", tuples=tuples)		
+			else:
+				return render_template("follow.html", tuples=tuples)		
 
 
 # @app.route("/delete", methods=['POST'])
@@ -285,9 +285,9 @@ def follow():
 
 @app.route("/update", methods=['POST', 'GET'])
 def update():
-    	tuples = []
+	tuples = []
 	if request.method == 'POST':
-    		searchMovie = request.form['movieSearch']
+		searchMovie = request.form['movieSearch']
 		#movieID = request.form['movieID']
 		#userRating = request.form['userRating']
 		#rating = updateMovieRating(movieID, userRating)
@@ -296,16 +296,16 @@ def update():
 	else:
 		return render_template("update.html", tuples=None)
 
-@app.route("/rate/<movieID>", methods=['POST', 'GET'])
-def rate(movieID=None):
-	if request.method == 'POST':
-		userRating = request.form['userRating']
-		rating = updateMovieRating(movieID, userRating)
-		return render_template("rate.html", movieID=movieID, rating=rating)
-	else:
-		return render_template("rate.html", movieID=movieID, rating="")
+		@app.route("/rate/<movieID>", methods=['POST', 'GET'])
+		def rate(movieID=None):
+			if request.method == 'POST':
+				userRating = request.form['userRating']
+				rating = updateMovieRating(movieID, userRating)
+				return render_template("rate.html", movieID=movieID, rating=rating)
+			else:
+				return render_template("rate.html", movieID=movieID, rating="")
 
 
-if __name__ == "__main__":
-	app.secret_key = os.urandom(12)
-	app.run(host='0.0.0.0', port=5200, debug=True)
+				if __name__ == "__main__":
+					app.secret_key = os.urandom(12)
+					app.run(host='0.0.0.0', port=5200, debug=True)
