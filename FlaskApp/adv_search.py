8ec11793fa6caaa5dfa7bae1699866862968d4ab
Kathleen Patricia Schermerhorn <kscherme@dsg1.crc.nd.edu>
2017-12-12 16:21:06 -0500

    finally

--- FlaskApp/adv_search.py
+++ FlaskApp/adv_search.py
@@ -4,6 +4,68 @@ import MySQLdb
 import os
 
 
+def goodAdvSearch(titleBeginning, titleContains, beginningYear, endingYear, \
+			genre, actor, beginningRating, endingRating):	
+	tablesClause = '''SELECT m.movieID, m.title, m.year, r.Rating FROM Movies m INNER JOIN Ratings r ON m.movieID=r.MovieID'''
+	whereClause = 'WHERE '
+	doIncludeWhere = False;
+	if titleBeginning != '':
+		whereClause += 'm.title LIKE \'{}%\''.format(titleBeginning)
+		doIncludeWhere = True;
+	
+	if titleContains != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		whereClause += 'm.title LIKE \'%{}%\''.format(titleContains)
+		doIncludeWhere = True;
+
+	if beginningYear != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		whereClause += 'm.year >= {}'.format(beginningYear)
+		doIncludeWhere = True;
+
+	if endingYear != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		whereClause += 'm.year <= {}'.format(endingYear)
+		doIncludeWhere = True;
+	
+	if genre != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		tablesClause += ' INNER JOIN Genres g ON g.movieID=m.movieID'
+		whereClause += 'g.genre=\'{}\''.format(genre)
+		doIncludeWhere = True;
+
+	if actor != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		tablesClause += ' INNER JOIN AppearsIn ai ON ai.movieID=m.movieID'
+		tablesClause += ' INNER JOIN Actors a ON a.actorID=ai.actorID'
+		whereClause += 'a.name like \'%{}%\''.format(actor)
+		doIncludeWhere = True;
+
+	if beginningRating != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		whereClause += 'rating >= {}'.format(beginningRating)
+		doIncludeWhere = True;
+
+	if endingRating != '':
+		if doIncludeWhere:
+			whereClause += ' AND '
+		whereClause += 'rating <= {}'.format(endingRating)	
+		doIncludeWhere = True;
+	
+
+	sql = tablesClause
+	if doIncludeWhere:
+		whereClause += ' GROUP BY m.movieID;'
+		sql += " " + whereClause
+
+	print(sql)
+	 
 
 def getAdvSearchResults(titleBeginning, titleContains, beginningYear, endingYear, \
 			genre, actor, beginningRating, endingRating):
@@ -63,4 +125,5 @@ def getAdvSearchResults(titleBeginning, titleContains, beginningYear, endingYear
 	print(sql)
 
 #getAdvSearchResults('begin', 'contains', 1900, 2000, 'Action', 'Brad Pitt', 5.0, 8.0)
-getAdvSearchResults('Up', '', 2005, 2017, 'Animation', 'Pete Docter', 1.0, 10.0)
+#getAdvSearchResults('Up', '', 2005, 2017, 'Animation', 'Pete Docter', 1.0, 10.0)
+goodAdvSearch('Up', '', 2005, 2017, 'Animation', 'Pete Docter', 1.0, 10.0)
